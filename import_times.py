0cae1859318480fbc828152e19012f2dda3c11e2
Daniel Porteous <dport@fb.com>
2019-08-03 11:28:05 +1000

    Add non-cumulative times and indentation

--- import_times.py
+++ import_times.py
@@ -9,24 +9,34 @@ import time
 
 
 _done = set()
+_import_level = 0
+_accumulated = 0
 
 
 @contextmanager
 def _timer(module_name):
+    global _done
+    global _import_level
+    global _accumulated
     if module_name in _done:
         yield
         return
     _done.add(module_name)
+    _import_level += 1
+    _accumulated = 0
     start = time.time()
     yield
     end = time.time()
     total = int((end - start) * 1000000)
+    _import_level -= 1
+    indentation = " " * _import_level * 2
     print(
-        "import time: {:9d} | {:10d} | {}".format(
-            0, total, module_name
+        "import time: {:9d} | {:10d} | {}{}".format(
+            total - _accumulated, total, indentation, module_name
         ),
         file=sys.stderr,
     )
+    _accumulated += total
 
 
 def _get_new_loader(f):
